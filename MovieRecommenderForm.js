6e0e00e29483de8d56953544a9e913257678c3e7
math92n4 <math92n4@stud.kea.dk>
2023-11-07 11:39:49 +0100

    commit

--- MovieRecommenderForm.js
+++ MovieRecommenderForm.js
@@ -4,39 +4,36 @@ const url = "http://localhost:8080/chat"; // Update the URL to match your backen
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("form");
 
+
+
     form.addEventListener("submit", async function (event) {
         event.preventDefault(); // Prevent the default form submission
 
         // Gather data from the form elements
         const selectedGenres = Array.from(document.querySelectorAll('input[type=checkbox]:checked')).map(checkbox => checkbox.value);
         const moviesAndActors = document.getElementById('multi-line-input').value;
-        const requestValue = `Genres: ${selectedGenres.join(', ')}; Movies & Actors: ${moviesAndActors}`;
+        const requestValue = `Movies & Actors: ${moviesAndActors} Genres: ${selectedGenres.join(', ')}`;
 
         // Create a JavaScript object with just the requestValue
         const request = {
+            selectedGenres: selectedGenres,
+            moviesAndActors: moviesAndActors,
             requestValue: requestValue
         };
 
-        console.log(request);
+        console.log(request.requestValue)
 
-        // Convert the JavaScript object to JSON
-        const jsonData = JSON.stringify(request);
 
-        // Send the JSON data to your backend using fetch
-        const response = await fetch(url + "/" + jsonData, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
+        const response = await fetch(url + "/" + request.requestValue)
+        const data = await response.json()
+        document.getElementById("answerBox").value = data[0].message.content
 
-        const answerBox = document.getElementById("answerBox")
-        const movieRecommendation = JSON.stringify(response);
-        console.log(JSON.stringify(movieRecommendation))
-        answerBox.textContent = movieRecommendation.message.content;
 
     });
 });
+
+
+
 
 
 
